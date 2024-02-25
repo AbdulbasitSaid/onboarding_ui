d186ed87c9e1d869ed27acf103873102115f8148
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2024-02-25 01:46:33 +0100

    Refactor variable names for clarity and consistency

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Onboarding Screen'),
     );
   }
 }
@@ -30,10 +32,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  // list of page view items
-  final pageViewItems = <Widget>[
-    const Center(
-      child: Text('Page 1'),
+  final List<Widget> pageViewItems = [
+    OnboardPageViewItem(
+      title: 'First page',
+      description: [
+        RichText(
+          text: const TextSpan(
+            text:
+                'Aliquip laborum sunt sint sint duis enim pariatur id excepteur. Voluptate dolore dolor quis nisi officia exercitation nulla Lorem amet. Aliqua fugiat Lorem Lorem culpa exercitation nostrud elit sunt do velit officia consectetur occaecat.',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        RichText(
+          text: const TextSpan(
+            text: 'Hello ',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                  text: 'bold', style: TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: ' world!'),
+            ],
+          ),
+        )
+      ],
+      imagePath: 'Thumbnail',
     ),
     const Center(
       child: Text('Page 2'),
@@ -51,21 +82,28 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       child: Text('Page 6'),
     ),
   ];
-  // progress state
+
   late AnimationController _progressController;
   late PageController _pageController;
+  final Curve _animationCurve = Curves.linear;
+
   @override
   void initState() {
     _pageController = PageController();
-
     _progressController = AnimationController(
       vsync: this,
       value: 1 / pageViewItems.length,
       duration: const Duration(milliseconds: 300),
       animationBehavior: AnimationBehavior.preserve,
     );
-
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _progressController.dispose();
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -79,7 +117,6 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         child: SizedBox(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          // color: Theme.of(context).colorScheme.primary,
           child: Stack(
             fit: StackFit.expand,
             children: [
@@ -95,53 +132,52 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                     ),
                   ),
                   Expanded(
-                      child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // progress indicator
-
-                      Stack(
-                        children: [
-                          Container(
-                            height: 10,
-                            width: MediaQuery.of(context).size.width,
-                            color: Colors.grey,
-                          ),
-                          AnimatedContainer(
-                            curve: Curves.easeIn,
-                            duration: const Duration(milliseconds: 300),
-                            height: 10,
-                            color: Colors.blue,
-                            width: MediaQuery.of(context).size.width *
-                                (_progressController.value),
-                          ),
-                        ],
-                      ),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          // previous and next buttons
-                          ElevatedButton(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Stack(
+                          children: [
+                            Container(
+                              height: 10,
+                              width: MediaQuery.of(context).size.width,
+                              color: Colors.grey,
+                            ),
+                            AnimatedContainer(
+                              curve: _animationCurve,
+                              duration: const Duration(milliseconds: 300),
+                              height: 10,
+                              color: Colors.blue,
+                              width: MediaQuery.of(context).size.width *
+                                  (_progressController.value),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            ElevatedButton(
                               onPressed: () => _handlePageChange(
-                                    _pageController.page!.round() - 1,
-                                  ),
-                              child: const Text("Previous")),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          ElevatedButton(
+                                _pageController.page!.round() - 1,
+                              ),
+                              child: const Text("Previous"),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            ElevatedButton(
                               onPressed: () {
                                 _handlePageChange(
                                   _pageController.page!.round() + 1,
                                 );
                               },
-                              child: const Text("Next")),
-                        ],
-                      ),
-                    ],
-                  )),
+                              child: const Text("Next"),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
               AnimatedPositioned(
@@ -149,9 +185,13 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                     (_progressController.value - 0.1),
                 top: MediaQuery.of(context).size.height * 0.72,
                 duration: const Duration(milliseconds: 300),
-                curve: Curves.easeIn,
-                child: const Text('car'),
-              )
+                curve: _animationCurve,
+                child: Container(
+                  color: Colors.purple,
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: const Text('car'),
+                ),
+              ),
             ],
           ),
         ),
@@ -161,13 +201,44 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   void _handlePageChange(int page) {
     setState(() {
-      _pageController.animateToPage(page,
-          duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
-      //
+      _pageController.animateToPage(
+        page,
+        duration: const Duration(milliseconds: 300),
+        curve: _animationCurve,
+      );
       _progressController.value =
           (_pageController.page!.round() + 1) / pageViewItems.length;
       print(_pageController.page!.round());
       print(_progressController.value);
     });
+  }
+}
+
+class OnboardPageViewItem extends StatelessWidget {
+  const OnboardPageViewItem({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.imagePath,
+  });
+
+  final String title;
+  final List<Widget> description;
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Text(imagePath),
+        ),
+        Text(title),
+        ...description,
+        const SizedBox(
+          height: 30,
+        ),
+      ],
+    );
   }
 }
